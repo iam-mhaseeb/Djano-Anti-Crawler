b68bd7c62b0db5b656354d3080df6b282cd5396f
Muhammad Haseeb <haseeb.emailbox@gmail.com>
2020-12-05 21:53:16 +0500

    Added bots whitelisting

--- django_anti_crawler/middlewares.py
+++ django_anti_crawler/middlewares.py
@@ -16,31 +16,34 @@ class DjangoAntiCrawlerMiddleware(object):
         x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
         ip = x_forwarded_for.split(',')[0] if x_forwarded_for else request.META.get('REMOTE_ADDR')
 
-        ip_cache_key = "django_anti_crawler:ip_rate" + ip
+        whitelisted_bots = settings.ANTI_CRAWLER_WHITELIST_BOTS if hasattr(settings, 'ANTI_CRAWLER_WHITELIST_BOTS') else []
 
-        ip_hits_timeout = settings.IP_HITS_TIMEOUT if hasattr(settings, 'IP_HITS_TIMEOUT') else 60
-        max_allowed_hits = settings.MAX_ALLOWED_HITS_PER_IP if hasattr(settings, 'MAX_ALLOWED_HITS_PER_IP') else 2000
+        if not ip in whitelisted_bots:
 
-        # get the hits by this IP in last IP_TIMEOUT time
-        this_ip_hits = cache.get(ip_cache_key)
+            ip_cache_key = "django_anti_crawler:ip_rate" + ip
 
-        if not this_ip_hits:
-            this_ip_hits = 1
-            cache.set(ip_cache_key, this_ip_hits, ip_hits_timeout)
-        else:
-            this_ip_hits += 1
-            cache.set(ip_cache_key, this_ip_hits)
+            ip_hits_timeout = settings.IP_HITS_TIMEOUT if hasattr(settings, 'IP_HITS_TIMEOUT') else 60
+            max_allowed_hits = settings.MAX_ALLOWED_HITS_PER_IP if hasattr(settings, 'MAX_ALLOWED_HITS_PER_IP') else 2000
 
-        log.info('Ip: {} already did: {} where max allowed hits are: {} and timeout for hits is: {}.'.format(
-          ip,
-          this_ip_hits,
-          max_allowed_hits,
-          ip_hits_timeout
-        ))
+            # get the hits by this IP in last IP_TIMEOUT time
+            this_ip_hits = cache.get(ip_cache_key)
 
-        if this_ip_hits > max_allowed_hits:
-            return HttpResponseForbidden()
+            if not this_ip_hits:
+                this_ip_hits = 1
+                cache.set(ip_cache_key, this_ip_hits, ip_hits_timeout)
+            else:
+                this_ip_hits += 1
+                cache.set(ip_cache_key, this_ip_hits)
 
-        else:
-            response = self.get_response(request)
-            return response
+            log.info('Ip: {} already did: {} where max allowed hits are: {} and timeout for hits is: {}.'.format(
+              ip,
+              this_ip_hits,
+              max_allowed_hits,
+              ip_hits_timeout
+            ))
+
+            if this_ip_hits > max_allowed_hits:
+                return HttpResponseForbidden()
+
+        response = self.get_response(request)
+        return response
